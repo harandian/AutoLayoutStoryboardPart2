315b7142a0fcfb15b60ea9e66388ef5df0d645df
harandian <hharandian@gmail.com>
2017-08-01 13:43:10 -0700

    first commit

---
+++ AutoLayoutPart2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutPart2
+//
+//  Created by Hirad on 2017-08-01.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutPart2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutPart2
+//
+//  Created by Hirad on 2017-08-01.
+//  Copyright © 2017 Hirad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
